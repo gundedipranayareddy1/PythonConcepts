cf64c5ead4b5849ab74d89069e953186727bec22
pranaya <gpranaya20@gmail.com>
2021-04-28 09:56:05 +0530

    initial commit

---
+++ oops/inheritance.py
@@ -0,0 +1,29 @@
+
+class Person(object):
+    def __init__(self,gender):
+        self.gender=gender
+        #print(this.gender)
+
+    def getGender(self):
+        print(self.gender)
+
+class Male(Person):
+
+    def __init(self,male):
+        super.__init__(self,male)
+    def getGender(self):
+        #super.getGender()
+        print("Male")
+
+
+class Female(Person):
+    def __init(self,female):
+        super.__init__(self,female)
+    def getGender(self):
+        print("Female")
+
+m=Male("Maleparent")
+m.getGender()
+
+f=Female("Femaleparent")
+f.getGender()
